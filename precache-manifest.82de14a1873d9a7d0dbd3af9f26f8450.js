90b3061668f26e0da13dbfceb3edd68e8c8bc84f
Vipin M <vipin@hirecraft.in>
2019-12-31 15:42:19 +0530

    first commit

---
+++ precache-manifest.82de14a1873d9a7d0dbd3af9f26f8450.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c709e214d1d0b93535d3b561c3ef80c9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a1d50f134d855e0910b3",
+    "url": "/static/css/2.64a99b10.chunk.css"
+  },
+  {
+    "revision": "f76f182e372b0425b128",
+    "url": "/static/css/main.8e8a31f5.chunk.css"
+  },
+  {
+    "revision": "a1d50f134d855e0910b3",
+    "url": "/static/js/2.1656c778.chunk.js"
+  },
+  {
+    "revision": "28ac78fef8675bfe412d94015e05d458",
+    "url": "/static/js/2.1656c778.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f76f182e372b0425b128",
+    "url": "/static/js/main.5fad98c5.chunk.js"
+  },
+  {
+    "revision": "d403b7c0018a654bd709",
+    "url": "/static/js/runtime-main.6d0632db.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
